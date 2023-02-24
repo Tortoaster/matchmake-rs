270aa75a9b489f1263e645a4f39c5f3160eb1124
Rick van der Wal <rickvanderwalvis@gmail.com>
2023-02-24 13:27:15 +0100

    Implement matchmaking

--- server/src/matchmaker.rs
+++ server/src/matchmaker.rs
@@ -1,30 +1,59 @@
 use axum::extract::ws::WebSocket;
 use futures_util::stream::SplitStream;
 use lazy_static::lazy_static;
+use std::mem;
+use std::ops::DerefMut;
+use std::time::Duration;
 use tokio::sync::Mutex;
+use tokio::time;
 
 lazy_static! {
-    static ref WAITING: Mutex<Option<SplitStream<WebSocket>>> = Mutex::new(None);
+    static ref STATE: Mutex<MatchState> = Mutex::new(MatchState::Waiting);
 }
 
 #[derive(Copy, Clone, Debug)]
 pub struct Matchmaker {
-    waiting: &'static Mutex<Option<SplitStream<WebSocket>>>,
+    state: &'static Mutex<MatchState>,
 }
 
 impl Matchmaker {
     pub fn new() -> Self {
-        Matchmaker { waiting: &WAITING }
+        Matchmaker { state: &STATE }
     }
 
     pub async fn find_match(&self, recv: SplitStream<WebSocket>) -> SplitStream<WebSocket> {
-        let mut lock = self.waiting.lock().await;
-        if lock.is_some() {
-            lock.take().unwrap()
-        } else {
-            *lock = Some(recv);
-            drop(lock);
-            todo!()
+        let mut lock = self.state.lock().await;
+        match *lock {
+            MatchState::Waiting => {
+                *lock = MatchState::P1Joined(recv);
+                drop(lock);
+
+                loop {
+                    time::sleep(Duration::from_secs(1)).await;
+                    let mut lock = self.state.lock().await;
+                    if let MatchState::P2Joined(_) = *lock {
+                        match mem::replace(lock.deref_mut(), MatchState::Waiting) {
+                            MatchState::P2Joined(recv) => break recv,
+                            _ => unreachable!(),
+                        }
+                    }
+                }
+            }
+            MatchState::P1Joined(_) => {
+                match mem::replace(lock.deref_mut(), MatchState::P2Joined(recv)) {
+                    MatchState::P1Joined(recv) => recv,
+                    _ => unreachable!(),
+                }
+            }
+            MatchState::P2Joined(_) => unimplemented!(),
         }
     }
+}
+
+#[derive(Debug, Default)]
+enum MatchState {
+    #[default]
+    Waiting,
+    P1Joined(SplitStream<WebSocket>),
+    P2Joined(SplitStream<WebSocket>),
 }
